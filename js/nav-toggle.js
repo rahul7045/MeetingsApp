188256031cc0d1988b52ce1048650871a527d361
Rahul <rahul.pujari@publicissapient.com>
2022-08-09 17:23:23 +0530

    navbar togglr btn

--- js/nav-toggle.js
+++ js/nav-toggle.js
@@ -2,5 +2,5 @@ const menuToggler = document.getElementById( 'menu-toggler' );
 const leftNav = document.querySelector( '.left-nav' );
 
 menuToggler.onclick = function() {
-    leftNav.classList.toggle( 'd-sm-none' );
+    leftNav.classList.toggle( 'd-bg-block' );
 };
