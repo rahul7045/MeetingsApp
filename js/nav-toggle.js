1814e261fe9a49d4edc914515bbf0bdad3895e89
Rahul <rahul.pujari@publicissapient.com>
2022-08-10 12:22:44 +0530

    added filter page

--- js/nav-toggle.js
+++ js/nav-toggle.js
@@ -1,6 +1,7 @@
 const menuToggler = document.getElementById( 'menu-toggler' );
-const leftNav = document.querySelector( '.left-nav' );
+const leftNav = document.querySelector( 'ul' );
 
 menuToggler.onclick = function() {
     leftNav.classList.toggle( 'd-bg-block' );
 };
+
